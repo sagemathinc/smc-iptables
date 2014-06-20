de505462d694d91110dac142250baddd15d69636
William Stein <wstein@gmail.com>
2014-06-20 19:25:40 +0000

    enable ssh forwarding to compute node

--- create_rules.sh
+++ create_rules.sh
@@ -1,46 +1,6 @@
 #!/bin/bash
 
-blacklist=ip_blacklist
 whitelist=ip_whitelist
-
-# block ip addresses in blacklist file
-while read -r ip
-do
-    # skip comments
-    [[ $ip = \#* ]] && continue
-
-    # skip empty lines
-    [[ -z "${ip}" ]] && continue
-
-    # append rule
-    iptables -v -I FORWARD -s 192.168.122.0/24 -d $ip -j DROP
-done < "$blacklist"
-
-
-# block access to UW subnets (insert at start of chain)
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 128.208.0.0/16 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 128.95.0.0/16 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 192.42.145.0/24 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 198.48.0.0/16 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 192.26.136.0/24 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 140.142.0.0/16 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 205.175.0.0/16 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 69.91.128.0/17 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 108.179.128.0/18 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 173.250.128.0/17 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 65.212.179.160/27 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 65.61.117.128/27 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 66.45.176.184/29 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 66.45.163.112/28 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 23.25.134.216/29 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 173.160.204.24/29 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-
-# block access to all private networks, access must be explicitely allowed after this rule.
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 172.16.0.0/12 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 192.168.0.0/16 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-iptables -v -I FORWARD -s 192.168.122.0/24 -d 10.0.0.0/8 -m state --state NEW -j REJECT --reject-with icmp-net-prohibited
-
 
 # allow libvirt-generated subnet
 iptables -v -I FORWARD -s 192.168.122.0/24 -d 192.168.122.0/24 -j ACCEPT
@@ -57,6 +17,12 @@ do
     # append rule
     iptables -v -I FORWARD -s 192.168.122.0/24 -d $ip -j ACCEPT
 done < "$whitelist"
+
+# block all non-whitelisted traffic originating from VMs
+iptables -v -A FORWARD -s 192.168.122.0/24 -j REJECT --reject-with icmp-net-prohibited
+
+# forward port 2222 to compute node
+./forward_ssh_compute.sh
 
 
 
---
+++ forward_ssh_compute.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+vm_name=compute
+vm_network=192.168.122.0/24
+host_port=2222
+destination_port=22
+lease_file=/var/lib/libvirt/dnsmasq/default.leases
+
+# search dnsmasq lease file for vm
+lease_info=$(egrep "$vm_name" $lease_file)
+
+# quit with error code if search failed
+if [ $? -ne 0 ]; then
+    echo "Error: IP address not found for VM $vm_name." 
+    exit 1
+fi
+
+# extract ip address from lease information
+destination_ip=$(echo $lease_info | awk '{ print $3 }' )
+
+echo "Forwarding host port $host_port to $destination_ip:$destination_port."
+
+# allow forwarding to vm network
+iptables -v -I FORWARD -m state -d $vm_network --state NEW,RELATED,ESTABLISHED -j ACCEPT
+
+# forward port from host to vm
+iptables -v -t nat -I PREROUTING -p tcp --dport $host_port -j DNAT --to-destination $destination_ip:$destination_port
