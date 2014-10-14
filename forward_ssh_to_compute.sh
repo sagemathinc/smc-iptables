b67be5b1302f727861e8b8f0a0f3a1bb1c17adf0
William Stein <wstein@gmail.com>
2014-10-14 01:39:42 +0000

    revert recent changes to port forwarding, which broke other things *far worse*

--- forward_ssh_to_compute.sh
+++ forward_ssh_to_compute.sh
@@ -6,7 +6,6 @@ host_port=2222
 destination_port=22
 lease_file=/var/lib/libvirt/dnsmasq/default.leases
 
-
 # need host's public ip address, this uses sagedev.org domain
 # so those addresses need to be correct
 host_fqdn=$HOSTNAME.sagedev.org
@@ -35,8 +34,5 @@ echo "Host IP is $host_ip, forwarding port $host_port to $destination_ip:$destin
 # allow forwarding to vm network
 iptables -v -I FORWARD -m state -d $vm_network --state NEW,RELATED,ESTABLISHED -j ACCEPT
 
-# forward incoming packets to vm
-iptables -v -t nat -I PREROUTING -p tcp             --dport $host_port -j DNAT --to-destination $destination_ip:$destination_port
-
-# forward outgoing packets destined for host's external ip (makes forwarding work on host itself)
-iptables -v -t nat -I OUTPUT     -p tcp -d $host_ip --dport $host_port -j DNAT --to-destination $destination_ip:$destination_port
+# forward port from host to vm
+iptables -v -t nat -I PREROUTING -p tcp -d $host_ip --dport $host_port -j DNAT --to-destination $destination_ip:$destination_port
